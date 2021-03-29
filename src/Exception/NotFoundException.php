3ad9fa5681f14183a0e1b5aaf70dd571354b8db8
David Gallagher <david@thegallagher.net>
2021-03-29 11:43:40 +1100

    Add getPath() method to NotFoundException

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -10,12 +10,30 @@ use RuntimeException;
 class NotFoundException extends RuntimeException
 {
     /**
+     * Missing path.
+     *
+     * @var string
+     */
+    private string $path;
+
+    /**
      * Not Found Exception constructor.
      *
      * @param string $path
      */
     public function __construct(string $path)
     {
-        parent::__construct("Not found: {$path}");
+        parent::__construct("Path not found: {$path}");
+        $this->path = $path;
+    }
+
+    /**
+     * Get the missing path.
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
     }
 }
